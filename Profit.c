328d366b29f8697dcc1798ccd0802c86dab93054
Yaswanthjangila <119721154+Yaswanthjangila@users.noreply.github.com>
2023-01-04 06:55:26 +0530

    #include<stdio.h> int main() {     int a,n;     scanf("%d%d",&a,&n);     if(a=n&&a>n)     {         printf("Loss");     }     else if(a=n&&a<n)     {         printf("Profit");     } }

---
+++ Profit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,n;
+    scanf("%d%d",&a,&n);
+    if(a=n&&a>n)
+    {
+        printf("Loss");
+    }
+    else if(a=n&&a<n)
+    {
+        printf("Profit");
+    }
+}
